41f7228522cbc660c36ebc6afb47eabd11548043
satoru akita <wwlap24@gmail.com>
2020-10-25 15:07:50 +0900

    opencv

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,ImageMessage
 )
+import cv2
+import numpy as np
+import random
 
 app = Flask(__name__)
 
@@ -20,6 +23,30 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+def change_kaonasi(test_url):
+    face_count=0
+    # カスケードファイルを指定して、分類機を作成
+    cascade_file = "haarcascade_frontalface_alt.xml"
+    cascade = cv2.CascadeClassifier(cascade_file)
+    img = cv2.imread(test_url)
+
+    # グレースケール変換
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # 顔を検知
+    faces = face_cascade.detectMultiScale(gray)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        face_count+=1
+        img_kaonasi_url="./img/"+str(random.randrange(10))+".jpg"
+        img_kaonasi = cv2.read(img_kaonasi_url)
+    
+        # 顔に合ったサイズに隠す用画像をリサイズする
+        img_kaonasi2 = img_kaonasi.resize((w, h))
+        img[0:height, 0:width] = img_kaonasi2
+
+    return img,face_count
 
 @app.route("/")
 def hello_world():
@@ -42,11 +69,25 @@ def callback():
 
     return 'OK'
 
+#メッセージ受信時のイベント
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image_message(event):
+    message_content = line_bot_api.get_message_content(event.message.id)
+    with open("static/"+event.message.id+".jpg", "wb") as f:
+        f.write(message_content.content)
+
+        test_url = "./static/"+event.message.id+".jpg"
+        img,face_count=change_kaonasi(test_url)
+
+        text=str(face_count)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=text))
+
 
 if __name__ == "__main__":
 #    app.run()
